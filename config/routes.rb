db00cda2a01e9574c6c20bc42a0189452981a96f
Michael Christenson II <michael@deepwoodsbrigade.com>
2013-03-06 20:20:08 -0500

    removed devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 RebelOutpostFoundation::Application.routes.draw do
-  devise_for :users
-
-  devise_scope :user do
-    match "logout" => "devise/sessions#destroy", :as => "logout"  
-    match "login" => "devise/sessions#new", :as => "login"  
-    match "signup" => "devise/registrations#new", :as => "signup"
-  end
-
   resources :friends
   root to: 'pages#index'
-
 end
